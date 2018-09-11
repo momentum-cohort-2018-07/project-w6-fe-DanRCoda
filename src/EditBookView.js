719f51237e0c3118343f82e3b539f6acb83303fd
Clinton Dreisbach <clinton@dreisbach.us>
2018-09-11 09:57:09 -0400

    Do not save book until you click submit

--- src/EditBookView.js
+++ src/EditBookView.js
@@ -1,24 +1,49 @@
 import React from 'react'
 
+function updateField (obj, fieldName, value) {
+  const newObj = Object.assign({}, obj)
+  newObj[fieldName] = value
+  return newObj
+}
+
 class EditBookView extends React.Component {
   constructor (props) {
     super()
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleCancel = this.handleCancel.bind(this)
+
+    this.state = {
+      book: props.book
+    }
+  }
+
+  updateBook (field, newValue) {
+    const updatedBook = updateField(this.state.book, field, newValue)
+    this.setState({
+      book: updatedBook
+    })
   }
 
   handleSubmit (event) {
+    event.preventDefault()
+    this.props.updateBook(this.state.book.id, 'title', this.state.book.title)
+    this.props.setEditing(false)
+  }
+
+  handleCancel (event) {
     event.preventDefault()
     this.props.setEditing(false)
   }
 
   render (books) {
-    let { book } = this.props
+    let { book } = this.state
     return (
       <div>
         <input type='text' value={book.title} onChange={event => {
-          this.props.updateBook(book.id, 'title', event.target.value)
+          this.updateBook('title', event.target.value)
         }} />
         <button onClick={this.handleSubmit}>Submit</button>
+        <button onClick={this.handleCancel}>Cancel</button>
       </div>
     )
   }
